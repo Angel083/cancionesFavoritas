a02a2e301d36d549f25d3f5c61fa34ae8a02be07
AngelGM <cesargasca083@gmail.com>
2022-05-30 21:15:42 -0500

    Funcion eliminar completada

--- src/views/EditSong.js
+++ src/views/EditSong.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React ,{useState, useEffect} from 'react';
+import { useParams, Link, useNavigate} from 'react-router-dom';
+import Axios from './../../node_modules/axios/index';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
 const EditSong = () => {
-  const { id } = useParams
-  console.log(id);
+  const { id } = useParams();
+  const [item, setItem] = useState([]);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    Axios.get(`http://localhost:3001/api/v1/songs/${id}`)
+    .then(({data}) => {setItem(data); console.log(data);
+    })
+    // console.log(item)
+  }, [setItem, id]);
+  function handleChange () {
+    Axios.delete(`http://localhost:3001/api/v1/songs/${id}`)
+    navigate("/")
+  }
   
   return <>
-    <p>Hola mundo </p>
+    <Link to="/">
+      <button className='botton'>
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </button>
+    </Link>
+    {item.map((item) => (
+      <div className='item__contenedor' key={id}>
+        <h2>Información de:</h2>
+        <p>{item.titulo}</p>
+        <p>{item.grupo}</p>
+        <p>{item.anio}</p>
+        <p>{item.genero}</p>
+        <form onSubmit={handleChange}>
+          <button type='submit' className='button item__delete'>Eliminar canción</button>
+        </form>
+      </div>
+    ))}
   </>;
 }
 
